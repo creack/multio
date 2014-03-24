ec29e95d82c3f1be87a808c2176b445658ccc8be
Guillaume J. Charmes <guillaume@charmes.net>
2014-03-23 20:50:10 -0700

    Update example

--- example_test.go
+++ example_test.go
@@ -5,45 +5,57 @@ import (
 	"github.com/creack/multio"
 	"io"
 	"log"
+	"os"
 )
 
 func ExampleMultiplexer() {
+	// Create two pipe pair to have both way read/write
+	// We could use socketpair, socket or other, but let's
+	// stick with pipes for now
 	r, w := io.Pipe()
 	r2, w2 := io.Pipe()
+
+	// Channel to wait on the goroutine before exit
 	ch := make(chan struct{})
 	go func() {
+		// When the goroutine finished, unlock (close chan)
 		defer close(ch)
+
+		// Create a side of the multiplexer
 		m, err := multio.NewMultiplexer(r, w2)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// Create two writers
 		wr := m.NewWriter(0)
-		fmt.Fprintf(wr, "Hello World!!!")
-
 		wr2 := m.NewWriter(1)
-		fmt.Fprintf(wr2, "Hello the World!!!")
+
+		// Send data and close.
+		// In this example, we use io.Copy which blocks until close
+		// If we do not close before write to wr2, the other goroutine
+		// will never read so it will block forever.
+		fmt.Fprintf(wr, "Hello World!!!\n")
+		wr.Close()
+		fmt.Fprintf(wr2, "Hello the World!!!\n")
+		wr2.Close()
 	}()
-	m, _ := multio.NewMultiplexer(r2, w)
 
-	var (
-		buf = make([]byte, multio.PageSize)
-		n   int
-		err error
-	)
+	// Create the other side of the multiplexer
+	m, err := multio.NewMultiplexer(r2, w)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Create two readers corresponding to our writers
 	rd := m.NewReader(0)
-	n, err = rd.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", buf[:n])
-
 	rd2 := m.NewReader(1)
-	n, err = rd2.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", buf[:n])
+
+	// Copy everything (until close) to stdout
+	io.Copy(os.Stdout, rd)
+	io.Copy(os.Stdout, rd2)
+
+	// Wait on the goroutine to finish
 	<-ch
 
 	// output:
